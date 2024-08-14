ee6f4decb3dfc0fd225709fd9db6cd52d3176ddb
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-13 22:17:10 -0300

    feat: creating getBeerById endpoint

--- src/main/java/dev/vini/openfeign_example/controller/BeerController.java
+++ src/main/java/dev/vini/openfeign_example/controller/BeerController.java
@@ -3,6 +3,7 @@ package dev.vini.openfeign_example.controller;
 import dev.vini.openfeign_example.client.BeerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class BeerController {
     @GetMapping
     public ResponseEntity getAllBeers(){
         return ResponseEntity.ok(beerClient.getAllBeers());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity getBeerById(@PathVariable("id") String id){
+        return ResponseEntity.ok(beerClient.getBeerById(id));
     }
 }
