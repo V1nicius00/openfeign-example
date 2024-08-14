38ac8a4bac49d6ef4566b81d352d0085269a7741
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-13 21:48:34 -0300

    feat: enabling feign client

--- src/main/java/dev/vini/openfeign_example/OpenfeignExampleApplication.java
+++ src/main/java/dev/vini/openfeign_example/OpenfeignExampleApplication.java
@@ -2,8 +2,10 @@ package dev.vini.openfeign_example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OpenfeignExampleApplication {
 
 	public static void main(String[] args) {
