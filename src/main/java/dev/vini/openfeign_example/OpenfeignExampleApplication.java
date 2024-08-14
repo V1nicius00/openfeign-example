113bf2d13a44bcf3c4cc346eed8e2afe393d8b44
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-13 21:24:12 -0300

    first commit

---
+++ src/main/java/dev/vini/openfeign_example/OpenfeignExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.vini.openfeign_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfeignExampleApplication.class, args);
+	}
+
+}
