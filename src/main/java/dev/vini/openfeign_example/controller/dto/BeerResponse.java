9b2de09fd8282801d3ebd06bd636e86124e17fbf
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-13 21:49:36 -0300

    feat: creating BeerResponse

---
+++ src/main/java/dev/vini/openfeign_example/controller/dto/BeerResponse.java
@@ -0,0 +1,71 @@
+package dev.vini.openfeign_example.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BeerResponse {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("brewery_type")
+    private String breweryType;
+    @JsonProperty("city")
+    private String city;
+    @JsonProperty("country")
+    private String country;
+
+    public BeerResponse() {
+    }
+
+    public BeerResponse(String id, String name, String breweryType, String city, String country) {
+        this.id = id;
+        this.name = name;
+        this.breweryType = breweryType;
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreweryType() {
+        return breweryType;
+    }
+
+    public void setBreweryType(String breweryType) {
+        this.breweryType = breweryType;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
