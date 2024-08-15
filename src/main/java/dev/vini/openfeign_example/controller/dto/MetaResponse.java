9672e1b0d98a64cacdf53c270c93bec894017650
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-14 22:10:20 -0300

    feat: creating MetaResponse dto

---
+++ src/main/java/dev/vini/openfeign_example/controller/dto/MetaResponse.java
@@ -0,0 +1,36 @@
+package dev.vini.openfeign_example.controller.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class MetaResponse {
+
+    private String total;
+
+    private String perPage;
+
+    public MetaResponse() {
+    }
+
+    public MetaResponse(String total, String perPage) {
+        this.total = total;
+        this.perPage = perPage;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(String perPage) {
+        this.perPage = perPage;
+    }
+}
